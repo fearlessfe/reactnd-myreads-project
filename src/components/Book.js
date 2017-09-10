4679826e09aa0cf62610631af0b52506aee0cd36
fearlessfe <505380967@qq.com>
2017-09-10 13:22:22 +0800

    change to meet some suggestions

--- src/components/Book.js
+++ src/components/Book.js
@@ -37,7 +37,7 @@ class Book extends React.Component {
         <li key={book.title}>
           <div className="book">
             <div className="book-top">
-                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                     <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                      <div className="book-shelf-changer">
                        <select 
                         value={shelf}
@@ -52,7 +52,7 @@ class Book extends React.Component {
                      </div>
             </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
+            <div className="book-authors">{book.authors?book.authors.join(','):''}</div>
           </div>
         </li>
 
